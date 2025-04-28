ab45ca4f49dce7fd9dc98f2e2a9edc1b38af8411
Tu Nombre <tuemail@ejemplo.com>
2025-04-28 17:24:42 -0300

    find-minimum-operations-to-make-all-elements-divisible-by-three solution

---
+++ find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,20 @@
+
+let nums = [1,2,3,4];
+
+
+
+
+var minimumOperations = function(nums) {
+
+  let operations = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+
+    if (nums[i] % 3 !== 0) {
+      operations += 1; 
+    } 
+  }
+  return operations;
+};
+
+minimumOperations(nums);
